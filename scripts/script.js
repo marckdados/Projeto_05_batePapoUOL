cb2ca397fbe47710a78b7040ee53be8eb536fe00
Bismarck <bismarckdados@gmail.com>
2022-04-18 17:34:50 -0300

    Adicionando a funcionalidade enviar mensagem

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,33 @@
-const nome = {
-    name:"Bisamar"
-}  
-function getApi(){
+let usuario;
+let tamanho;
+let conectado;
+let nome;
+
+function buscarMensagens(){
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    console.log(promise.then())
-    promise.then(exibirMensagem)
+    promise.then(exibirMensagem);
 }
 
+
+function usuarioEntrar(){
+    nome = prompt('Qual seu nome ?');
+    usuario = {name:nome};
+    const conectar = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',usuario)
+    conectar.then(buscarMensagens);
+    conectar.then(() => {
+         conectado = setInterval(manterConexao,4000);
+    });
+    conectar.catch(tratarErro);
+}
+
+function tratarErro(){
+    alert('Usuário já conectado, logue com outro nome.');
+    usuarioEntrar();
+}
+
+
 function exibirMensagem(resposta){
-    let lugarMensagens = document.querySelector('.section');
-    console.log(resposta)
+    const lugarMensagens = document.querySelector('.section');
     for( i = 0; i < resposta.data.length; i++){
         lugarMensagens.innerHTML += `
         <div class="caixa-mensagem ${resposta.data[i].type}">
@@ -18,29 +36,51 @@ function exibirMensagem(resposta){
                 <div class="mensagem">${resposta.data[i].text}</div>
         </div>
         `;
-        
     }
-
+    setInterval(atualizarMensagens,3000);
 }
 
-function entrarSala(){
-    
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',nome);
 
-}
 
 function manterConexao(){
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/status',nome);
-    console.log('to sendo chamado')
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status',usuario);
+    promise.then(console.log('Deu certo'));
 }
 
 
-function enviarMensagens(){
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/messages')
+function atualizarMensagens(){
+    const promesa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promesa.then(novasMensagens);
+    
 }
 
-setInterval(manterConexao,5000);
+function novasMensagens(resposta){
+    tamanho = resposta.data.length -1;
+    const novaMensagem = document.querySelector('.section');
+            novaMensagem.innerHTML += 
+            `<div class="caixa-mensagem ${resposta.data[tamanho].type}">
+            <div class="timer">${resposta.data[tamanho].time}</div>
+            <div class="nome">${resposta.data[tamanho].from}</div>
+            <div class="mensagem">${resposta.data[tamanho].text}</div>
+            </div>
+            `;
+}
 
+function enviarMensagens(){ 
+    let textoInput = document.querySelector("input").value;
+    console.log(textoInput)
+    const mensagem = {
+        from: nome,
+        to: "Todos",
+        text: textoInput,
+        type: "message"
+    }
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',mensagem);
+    promise.then(console.log('Mensagem Enviada'));
+    promise.catch(()=>{
+        window.location.reload();
+    });
 
-entrarSala();
-getApi();
+}
+
+usuarioEntrar();
